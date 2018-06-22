ed6378bbcbf6925c27626770314854c55ee34606
tmirun <tmirun@hotmail.com>
2018-06-22 13:59:02 +0200

    build post detail page

--- src/app/pages/dashboard/post/post-detail/post-detail.component.ts
+++ src/app/pages/dashboard/post/post-detail/post-detail.component.ts
@@ -1,15 +1,74 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { PostService } from '../../../../services/post.service';
+import { Subscription } from 'rxjs';
+import { Post } from '../../../../Models/Post.interface';
+import { ActivatedRoute, Router } from '@angular/router';
+import 'rxjs/add/operator/filter';
+import 'rxjs/add/operator/switchMap';
+import * as moment from 'moment';
+import {
+  FormBuilder,
+  FormGroup,
+  Validators
+} from '@angular/forms';
 
 @Component({
   selector: 'app-post-detail',
   templateUrl: './post-detail.component.html',
   styleUrls: ['./post-detail.component.scss']
 })
-export class PostDetailComponent implements OnInit {
+export class PostDetailComponent implements OnInit, OnDestroy {
 
-  constructor() { }
+  public form: FormGroup;
+  public post: Post;
+  public raw: string;
+  public title: string;
+  public tags: string;
+  public categories: string;
+  public toc: boolean;
+  public path: string;
+  public date: moment.Moment;
+
+  public routeSubscription: Subscription;
+
+  constructor(
+    private postService: PostService,
+    private route: ActivatedRoute,
+    private router: Router,
+    private fb: FormBuilder
+  ) {
+    this.form = this.fb.group({
+      raw:  [ this.raw, [ Validators.required ] ]
+    });
+  }
 
   ngOnInit() {
+    this.routeSubscription = this.route.params
+      .switchMap(params => {
+        return this.postService.posts$
+          .map(posts => posts.find(post =>  post._id === params.id));
+      })
+      .subscribe((post) => {
+        if (!post) {
+          this.router.navigate(['/dashboard/post']);
+          return ;
+        }
+        this.post = post;
+        this.raw = this.post.raw;
+        this.path = this.post.path;
+
+        this.form.setValue({
+          raw: this.raw,
+        });
+      });
+  }
+
+  ngOnDestroy() {
+    this.routeSubscription.unsubscribe();
+  }
+
+  submitForm() {
+    // this.postService.update(this.post.path);
   }
 
 }
