d2af5afb990d85d76c3bc5641c5610ade05d5a29
tmirun <tmirun@hotmail.com>
2018-08-13 18:47:01 +0200

    Add Start Server to sidebar && add Tooltips

--- src/app/components/sidebar/sidebar.component.ts
+++ src/app/components/sidebar/sidebar.component.ts
@@ -4,7 +4,8 @@ import { NzModalRef, NzModalService } from 'ng-zorro-antd';
 import { UtilsService } from './../../services/utils.service';
 import { NzMessageService } from 'ng-zorro-antd';
 import { Subscription } from 'rxjs';
-import {HexoService} from '../../services/hexo.service';
+import { HexoService } from '../../services/hexo.service';
+import { ServerService } from '../../services/server.service';
 
 @Component({
   selector: 'app-sidebar',
@@ -22,7 +23,8 @@ export class SidebarComponent implements OnInit {
     private modalService: NzModalService,
     private utilsService: UtilsService,
     private hexoService: HexoService,
-    private message: NzMessageService
+    private message: NzMessageService,
+    public serverService: ServerService
   ) {
   }
 
@@ -52,5 +54,17 @@ export class SidebarComponent implements OnInit {
       this.message.remove(deployMessageId);
       this.message.error(`DEPLOY ERROR ${error}`);
     });
+  }
+
+  public switchServer() {
+    if (this.serverService.isServerRunning) {
+      this.serverService.stopServer();
+      this.message.info('STOP SERVER');
+    } else {
+      this.message.info('STARTING SERVER');
+      this.serverService.startServer().catch((error) => {
+        this.message.error(`SERVER STARTING ERROR ${error}`);
+      });;
+    }
   }
 }
