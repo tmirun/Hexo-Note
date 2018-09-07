a05dfda2f9968017b327b987915868c39776851c
tmirun <tmirun@hotmail.com>
2018-09-07 23:17:49 +0200

    change is url check regex

--- src/app/pages/dashboard/post/post-detail/post-detail.component.ts
+++ src/app/pages/dashboard/post/post-detail/post-detail.component.ts
@@ -271,12 +271,7 @@ export class PostDetailComponent implements OnInit, OnDestroy, CanDeactivateGuar
   }
 
   private _isURL(str) {
-    const pattern = new RegExp('^(https?:\\/\\/)?' + // protocol
-      '((([a-z\\d]([a-z\\d-]*[a-z\\d])*)\\.?)+[a-z]{2,}|' + // domain name
-      '((\\d{1,3}\\.){3}\\d{1,3}))' + // OR ip (v4) address
-      '(\\:\\d+)?(\\/[-a-z\\d%_.~+]*)*' + // port and path
-      '(\\?[;&a-z\\d%_.~+=-]*)?' + // query string
-      '(\\#[-a-z\\d_]*)?$', 'i'); // fragment locator
+    const pattern = /(ftp|http|https):\/\/(\w+:{0,1}\w*@)?(\S+)(:[0-9]+)?(\/|\/([\w#!:.?+=&%@!\-\/]))?/
     return pattern.test(str);
   }
 }
